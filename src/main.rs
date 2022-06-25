cc7a2c624900ed270554730c19a0d29776b872c5
tom1236908745 <tom1233908745@gmail.com>
2022-06-25 15:48:49 +0900

    create: Nicer error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
-#![allow(unused)]
+use anyhow::{Context, Result};
 
-use clap::Parser;
-
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
-
-fn main() {
-
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
-
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }
