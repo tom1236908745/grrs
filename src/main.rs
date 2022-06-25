425949b49853b31666907fd455520af30fbccda9
tom1236908745 <tom1233908745@gmail.com>
2022-06-25 18:17:54 +0900

    add: テストコードの追加

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,37 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let input = &args[1];
 
-    let decoded = percent_decode(input.as_bytes()).decode_utf8();
-    println!("{}", decoded.unwrap());
+    println!("{}", decode(input));
+}
+
+fn decode(input: &str) -> String {
+    percent_decode(input.as_bytes()).decode_utf8().unwrap().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::decode;
+
+    #[test]
+    fn decode_space_ok() {
+        let expected = "foo bar";
+        let input = "foo%20bar";
+        let actual = decode(input);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn decode_ascii_ok() {
+        let expected = r##" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
+        let input = r##"%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
+        let actual = decode(input);
+        assert_eq!(expected, actual);
+    }git 
+
+    #[test]
+    #[should_panic]
+    fn decode_invalid_utf8_ng() {
+        let input = "%93%FA%96%7B%8C%EA%0D%0A";
+        decode(input);
+    }
 }
